465fe08ef267d949ab3a724ba2a91cfcd5376c0f
Alice <alicehata9@gmail.com>
2023-04-17 17:22:54 -0300

    add validation error handling

--- src/main/java/com/alice/crudspring/controller/ApplicationControllerAdvice.java
+++ src/main/java/com/alice/crudspring/controller/ApplicationControllerAdvice.java
@@ -1,6 +1,8 @@
 package com.alice.crudspring.controller;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -14,5 +16,12 @@ public class ApplicationControllerAdvice {
   @ResponseStatus(HttpStatus.NOT_FOUND)
   public String handleNotFoundException(RecordNotFoundException ex) {
     return "Error: " + ex.getMessage();
+  }
+
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
+  public String handleMethodArgumentNotValid(MethodArgumentNotValidException ex) {
+    FieldError error = ex.getBindingResult().getFieldErrors().get(0);
+    return "Error: " + error.getDefaultMessage();
   }
 }
